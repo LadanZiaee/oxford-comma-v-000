072323bc51d0b54b5d1f8a00aa4e1ba97b14740f
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-07-01 18:48:05 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,4 @@
 def oxford_comma(array)
-  if array.count == 1
-    array.join
-    elsif array.count == 2 
-    array.join(" and ")
-    elsif array.count == 3
-    array.join(" , ", " and ")
+  if array == 1 
   end
 end
