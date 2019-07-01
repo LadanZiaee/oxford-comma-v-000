e3d41cbdb742b56ecad2ffe532fe33d6fa505ceb
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-07-01 18:35:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,6 @@
 def oxford_comma(array)
-  array.join
-  if array.i = 1 
-    array
-    elsif array.i = 2 
+  if array.to_i == 1
+    array.join
+    elsif array.to_i == 2 
     array.join("and")
-    elsif array.i = 3
-    array.join(",", "and")
-  else
-    array.join(",", "and")
-  end
 end
